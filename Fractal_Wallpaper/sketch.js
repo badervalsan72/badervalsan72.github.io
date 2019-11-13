297ed1aa956509d12edd6a58885664badc0892ba
badervalsan72 <ebadillav27@gmail.com>
2019-11-12 22:25:13 -0600

    agregando bosque

--- Fractal_Wallpaper/sketch.js
+++ Fractal_Wallpaper/sketch.js
@@ -1,18 +1,10 @@
-var bosque = []; // para guardar los arboles
-var arbol = []; // para guardar las ramas de cada arbol 
+let bosque = []; // para guardar los arboles
 
 function setup() {
     createCanvas(windowWidth, windowHeight); 
-    background(0);  // podría poner un background aleatorio como para el cielo 
+    background(0, random(0, 255), random(0, 255));  // podría poner un background aleatorio como para el cielo 
+    hacerArboles(); 
     
-    let pos = random(0, width); 
-    let a = createVector(pos, height); 
-    let b =createVector(pos, height/1.1); 
-    base = new Branch(a, b, 5); 
-    
-    arbol[0] = base; 
-    arbol[1] = base.createBranchDer(); 
-    arbol[2] = base.createBranchIzq(); 
     
 
 }
@@ -22,29 +14,57 @@ function setup() {
     * sun 
     * mountains 
     * arboles verticales 
-    * arbustos? 
+    * bosque de arboles mixtos! 
+    * arbustos? serían como mini arboles pero con varias bases 
     * ver esquemas de colores 
     * ver nature of code
 
 */
+function hacerArboles() 
+{
+    let numarbol = random(0,1) * 10; 
+    
+    for (let arbolin = 0; arbolin < numarbol; arbolin++)
+    {
+        let newArbol = []; 
+        let pos = random(0, width); 
+        let a = createVector(pos, height); 
+        let b =createVector(pos, height/1.1); 
+        newArbol[0] = new Branch(a, b, 5); 
+        newArbol[1] = newArbol[0].createBranchDer(); 
+        newArbol[2] = newArbol[0].createBranchIzq(); 
+        bosque[arbolin] = newArbol; 
+        
+
+    }
+
+
+}
 function mousePressed()
 {
-    for (let i = arbol.length -1; i > 0; i--)
+    for (let i = 0; i < bosque.length; i++)
     {
-        if (!arbol[i].acabado)
+        for (let j = bosque[i].length -1; j > 0; j--)
         {
+            if (!bosque[i][j].acabado)
+            {
 
-            arbol.push(arbol[i].createBranchDer());
-            arbol.push(arbol[i].createBranchIzq());
+                bosque[i].push(bosque[i][j].createBranchDer());
+                bosque[i].push(bosque[i][j].createBranchIzq());
+            }
+            bosque[i][j].acabado = true; 
         }
-        arbol[i].acabado = true; 
     }
+    
 
 }
 function draw() {
-    for (let i = 0; i < arbol.length; i++) 
+    for (let i = 0; i < bosque.length; i++)
     {
-        arbol[i].show(); 
+        for (let j = 0; j < bosque[i].length; j++)
+        {
+            bosque[i][j].show()
+        } 
     }
       
    
